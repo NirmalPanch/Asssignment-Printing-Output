4be580d114e92651fdee0c28bb1abea1bdaef2f2
NirmalPanch <169159459+NirmalPanch@users.noreply.github.com>
2024-05-21 22:40:45 -0400

    added js file

---
+++ index.js
@@ -0,0 +1,46 @@
+let a = 3;
+let b = 5;
+let c;
+
+console.log(
+      "==========" + "\n" +
+      "Task 1" + "\n" +
+      "==========" + "\n" +
+
+      "let a = 3;" + "\n" +
+      "let b = 5;" + "\n" +
+      "let c;" + "\n" +
+      "-----------" + "\n" +
+      "a + b: " + (a + b) + "\n" +
+      "a - b: " + (a - b) + "\n" +
+      "a * b: " + (a * b) + "\n" +
+      "a / b: " + (a / b) + "\n" +
+      "a % b: " + (a % b) + "\n" +
+      "a += b: " + (a += b) + "\n" +
+      "a -= b: " + (a -= b) + "\n" +
+      "a *= b: " + (a *= b) + "\n" +
+      "a /= b: " + (a /= b) + "\n" +
+      "a %= b: " + (a %= b) + "\n" +
+      "a == b: " + (a == b) + "\n" +
+      "a != b: " + (a != b) + "\n" +
+      "a > b: " + (a > b) + "\n" +
+      "a < b: " + (a < b) + "\n" +
+      "!a && !c: " + (!a && !c) + "\n" +
+      "!a || !c: " + (!a || !c));
+
+
+
+
+let first_name = "Nirmal";
+let last_name = "Panchal";
+let email = "panchalnirmal306@gmail.com";
+
+// Create the expression using template literals
+let output = `"My name is ${first_name} ${last_name}. You can contact me at ${email}."`;
+
+// Print the output in the browser's console
+console.log( "\n" +"\n" +
+    "==========" + "\n" +
+    "Task 2" + "\n" +
+    "==========" + "\n" );
+console.log(output);
