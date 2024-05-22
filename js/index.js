7e39101c076dc62e13d9a014b022cc72a32539bf
NirmalPanch <169159459+NirmalPanch@users.noreply.github.com>
2024-05-21 23:03:02 -0400

    Update index.js comments

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 let a = 3;
 let b = 5;
 let c;
-
+//printing Task 1 output
 console.log(
       "==========" + "\n" +
       "Task 1" + "\n" +
@@ -35,10 +35,10 @@ let first_name = "Nirmal";
 let last_name = "Panchal";
 let email = "panchalnirmal306@gmail.com";
 
-// Create the expression using template literals
+// Creating the expression
 let output = `"My name is ${first_name} ${last_name}. You can contact me at ${email}."`;
 
-// Print the output in the browser's console
+// Printing the output
 console.log( "\n" +"\n" +
     "==========" + "\n" +
     "Task 2" + "\n" +
